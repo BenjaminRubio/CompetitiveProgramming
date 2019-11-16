6e1c9dafa56ddf387b95bcec90d6357b1572ce39
BenjaminRubio <berubio@uc.cl>
2019-11-15 23:36:21 -0300

    update

--- Problems/Otros/DazzlingStars.cpp
+++ Problems/Otros/DazzlingStars.cpp
@@ -2,56 +2,9 @@
 using namespace std;
 
 typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> par;
-typedef vector<int> vi;
-typedef vector<float> vfloat;
-typedef vector<par> vp;
-typedef vector<vi> graph;
-typedef vector<vp> wgraph;
-
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
 #define repx(i, a, b) for (int i = a; i < (int)b; i++)
-#define invrep(i, b, a) for (int i = b; i >= a; --i)
-
 #define pb push_back
-#define pf push_front
-#define eb emplace_back
-#define ppb pop_back
-
-#define umap unordered_map
-
-#define lg(x) (31 - __builtin_clz(x))
-#define lgg(x) (63 - __builtin_clzll(x))
-#define gcd __gcd
-
-//ios::sync_with_stdio(0); cin.tie(0);
-//cout.setf(ios::fixed); cout.precision(4);
-
-#define debugx(x) cerr << #x << ": " << x << endl
-#define debugv(v)         \
-    cerr << #v << ":";    \
-    for (auto e : v)      \
-        cerr << " " << e; \
-    cerr << endl
-#define debugm(m)                                        \
-    cerr << #m << endl;                                  \
-    rep(i, (int)m.size())                                \
-    {                                                    \
-        cerr << i << ":";                                \
-        rep(j, (int)m[i].size()) cerr << " " << m[i][j]; \
-        cerr << endl;                                    \
-    }
-#define debugmp(m) //\
-    cerr << #m << endl;                                                                         \
-    rep(i, (int)m.size())                                                                       \
-    {                                                                                           \
-        cerr << i << ":";                                                                       \
-        rep(j, (int)m[i].size()) cerr << " {" << m[i][j].first << "," << m[i][j].second << "}"; \
-        cerr << endl;                                                                           \
-    }
-#define print(x) copy(x.begin(), x.end(), ostream_iterator<int>(cout, “”)), cout << endl
-
 const double PI = 3.14159265358979323846;
 
 template <typename T>
