f0cebf810e31eee3e05176de0680f3c5e07cd0ef
BenjaminRubio <berubio@uc.cl>
2019-11-22 20:04:05 -0300

    update 22/11/2019

--- Problems/URI/LookingForTheRiskFactor.cpp
+++ Problems/URI/LookingForTheRiskFactor.cpp
@@ -1,76 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> par;
 typedef vector<int> vi;
-typedef vector<float> vfloat;
-typedef vector<par> vp;
-typedef vector<vi> graph;
-typedef vector<vp> wgraph;
+typedef vector<int>::iterator iter;
 
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
 #define repx(i, a, b) for (int i = a; i < (int)b; i++)
-#define invrep(i, a, b) for (int i = a; i > (int)b; i--)
 
-#define pb push_back
-#define pf push_front
-#define eb emplace_back
-#define ppb pop_back
+class WaveTree
+{
+    vector<vector<int>> r0;
+    int n, s;
+    vector<int> arrCopy;
 
-#define umap unordered_map
-
-#define lg(x) (31 - __buitlin_clz(x))
-#define lgg(x) (63 - __buitlin_clzll(x))
-#define gcd __gcd
-
-//ios::sync_with_stdio(0); cin.tie(0);
-//cout.setf(ios::fixed); cout.precision(4);
-
-#define debugx(x) // cerr << #x << ": " << x << endl
-#define debugv(v)         \
-    cerr << #v << ":";    \
-    for (auto e : v)      \
-        cerr << " " << e; \
-    cerr << endl
-#define debugm(m)                                        \
-    cerr << #m << endl;                                  \
-    rep(i, (int)m.size())                                \
-    {                                                    \
-        cerr << i << ":";                                \
-        rep(j, (int)m[i].size()) cerr << " " << m[i][j]; \
-        cerr << endl;                                    \
+    void build(iter b, iter e, int l, int r, int u)
+    {
+        if (l == r)
+            return;
+        int m = (l + r) / 2;
+        r0[u].reserve(e - b + 1);
+        r0[u].push_back(0);
+        for (iter it = b; it != e; ++it)
+            r0[u].push_back(r0[u].back() + (*it <= m));
+        iter p = stable_partition(b, e, [=](int i) { return i <= m; });
+        build(b, p, l, m, u * 2);
+        build(p, e, m + 1, r, u * 2 + 1);
     }
-#define debugmp(m) //                                                                       \
-    cerr << #m << endl;                                                                         \
-    rep(i, (int)m.size())                                                                       \
-    {                                                                                           \
-        cerr << i << ":";                                                                       \
-        rep(j, (int)m[i].size()) cerr << " {" << m[i][j].first << "," << m[i][j].second << "}"; \
-        cerr << endl;                                                                           \
+
+    int q, w;
+    int range(int a, int b, int l, int r, int u)
+    {
+        if (r < q or w < l)
+            return 0;
+        if (q <= l and r <= w)
+            return b - a;
+        int m = (l + r) / 2, za = r0[u][a], zb = r0[u][b];
+        return range(za, zb, l, m, u * 2) +
+               range(a - za, b - zb, m + 1, r, u * 2 + 1);
     }
-#define print(x) copy(x.begin(), x.end(), ostream_iterator<int>(cout, “”)), cout << endl
+
+public:
+    WaveTree(vector<int> arr, int sigma)
+    {
+        n = arr.size();
+        s = sigma;
+        r0.resize(s * 2);
+        arrCopy = arr;
+        build(arr.begin(), arr.end(), 0, s - 1, 1);
+    }
+
+    int range(int x, int y, int a, int b)
+    {
+        if (y < x or b <= a)
+            return 0;
+        q = x;
+        w = y;
+        return range(a, b, 0, s - 1, 1);
+    }
+};
 
 const int MAX_NUM = 1e5;
 int q, n, k;
-vi rem, max_prime, primes;
-graph order;
-
-int binsearch(int ind, int n)
-{
-    int i = 0;
-    int j = order[ind].size();
-    while (i < j)
-    {
-        int m = (i + j) >> 1;
-        if (order[ind][m] > n)
-            j = m;
-        else
-            i = m + 1;
-    }
-    return i;
-}
+vi max_prime;
 
 int main()
 {
@@ -78,40 +69,10 @@ int main()
     cin.tie(0);
 
     max_prime.assign(MAX_NUM + 1, 0);
-    primes.assign(MAX_NUM + 1, -1);
-    rem.assign(MAX_NUM + 1, -1);
-    int limit = floor(sqrt(MAX_NUM));
-    repx(i, 2, limit + 1) if (max_prime[i] == 0) for (int j = i * i; j <= MAX_NUM; j += i)
-    {
+    repx(i, 2, MAX_NUM + 1) if (max_prime[i] == 0) for (int j = i; j <= MAX_NUM; j += i)
         max_prime[j] = i;
-        if (rem[j] == -1)
-            n = j / i;
-        else
-            n = rem[j] / i;
-        while (n % i == 0)
-            n /= i;
-        rem[j] = n;
-    }
 
-    int n_primes = 0;
-
-    repx(i, 2, MAX_NUM + 1)
-    {
-        primes[i] = n_primes - 1;
-        if (max_prime[i] == 0)
-            primes[i] = n_primes++;
-    }
-
-    order.resize(n_primes + 1);
-
-    repx(i, 2, MAX_NUM + 1)
-    {
-        if (max_prime[i] == 0)
-            max_prime[i] = i;
-        max_prime[i] = max(max_prime[i], rem[i]);
-        // cerr << max_prime[i] << ' ' << primes[max_prime[i]] << '\n';
-        order[primes[max_prime[i]]].pb(i);
-    }
+    WaveTree wt(max_prime, MAX_NUM + 1);
 
     cin >> q;
     rep(i, q)
@@ -124,12 +85,7 @@ int main()
             continue;
         }
 
-        int ans = 0;
-        int prime_k = primes[k];
-        rep(j, prime_k + 1)
-        {
-            ans += binsearch(j, n);
-        }
+        int ans = wt.range(0, k, 2, n + 1);
 
         cout << ans << '\n';
     }
