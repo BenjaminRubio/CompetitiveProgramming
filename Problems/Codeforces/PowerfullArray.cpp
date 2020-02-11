489c93d8b95645e5d7ff47b58391e6f4232b6c11
Benjamin Rubio <BenjaR@localhost.localdomain>
2020-02-11 17:51:22 -0300

    update 11/02/2020

---
+++ Problems/Codeforces/PowerfullArray.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i< (int)n; i++)
+typedef long long ll;
+
+int sq(int x) { return sqrt(x); }
+
+struct Query
+{
+    int l, r, id;
+    Query(int l, int r, int id) : l(l), r(r), id(id) {}
+    bool operator<(Query &q)
+    {
+        return sq(l) < sq(q.l) or (sq(l) == sq(q.l) and sq(r) < sq(q.r));
+    }
+};
+
+int n, t, l, r;
+ll s;
+ll a[200010], ans[200010], c[1000010];
+vector<Query> q;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> t;
+
+    rep(i, n) cin >> a[i];
+
+    rep(i, t)
+    {
+        cin >> l >> r;
+        q.emplace_back(l - 1, r - 1, i);
+    }
+
+    sort(q.begin(), q.end());
+
+    l = 0;
+    r = 0;
+    s = a[0];
+    c[a[0]] = 1;
+    for (Query e : q)
+    {
+        while (r != e.r)
+        {
+            if (e.r > r)
+            {
+                c[a[r + 1]]++;
+                s += a[r + 1] * (2 * c[a[r + 1]] - 1);
+                r++;
+            }
+            else
+            {
+                s -= a[r] * (2 * c[a[r]] - 1);
+                c[a[r]]--;
+                r--;
+            }
+        }
+        while (l != e.l)
+        {
+            if (e.l > l)
+            {
+                s -= a[l] * (2 * c[a[l]] - 1);
+                c[a[l]]--;
+                l++;
+            }
+            else
+            {
+                c[a[l - 1]]++;
+                s += a[l - 1] * (2 * c[a[l - 1]] - 1);
+                l--;
+            }
+        }
+
+        ans[e.id] = s;
+    }
+
+    rep(i, t) cout << ans[i] << '\n';
+}
