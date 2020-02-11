62dc127108866f11cdab13cb6401de29070e957c
Benjamin Rubio <BenjaR@localhost.localdomain>
2020-02-10 22:23:10 -0300

    update 10/02/2020

--- Problems/URI/InvertingHuffman.cpp
+++ Problems/URI/InvertingHuffman.cpp
@@ -1,69 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> par;
-typedef vector<int> vi;
-typedef vector<float> vfloat;
-typedef vector<par> vp;
-typedef vector<vi> graph;
-typedef vector<vp> wgraph;
-
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
-#define repx(i, a, b) for (int i = a; i < (int)b; i++)
-#define invrep(i, a, b) for (int i = a; i > (int)b; i--)
+#define forall(it, arr) for (auto it = arr.begin(); it != arr.end(); it++)
+typedef long long ll;
 
-#define pb push_back
-#define pf push_front
-#define eb emplace_back
-#define ppb pop_back
-
-#define umap unordered_map
-
-#define lg(x) (31 - __builtin_clz(x))
-#define lgg(x) (63 - __builtin_clzll(x))
-#define gcd __gcd
-
-//ios::sync_with_stdio(0); cin.tie(0);
-//cout.setf(ios::fixed); cout.precision(4);
-
-#define debugx(x) // cerr << #x << ": " << x << endl
-#define debugv(v)         \
-    cerr << #v << ":";    \
-    for (auto e : v)      \
-        cerr << " " << e; \
-    cerr << endl
-#define debugm(m)  //                                  \
-    cerr << #m << endl;                                  \
-    rep(i, (int)m.size())                                \
-    {                                                    \
-        cerr << i << ":";                                \
-        rep(j, (int)m[i].size()) cerr << " " << m[i][j]; \
-        cerr << endl;                                    \
-    }
-#define debugmp(m) //                                                                       \
-    cerr << #m << endl;                                                                         \
-    rep(i, (int)m.size())                                                                       \
-    {                                                                                           \
-        cerr << i << ":";                                                                       \
-        rep(j, (int)m[i].size()) cerr << " {" << m[i][j].first << "," << m[i][j].second << "}"; \
-        cerr << endl;                                                                           \
-    }
-#define print(x) copy(x.begin(), x.end(), ostream_iterator<int>(cout, “”)), cout << endl
-
-int n;
-vi l, v;
+int n, l;
+vector<vector<ll>> arr;
 
 int main()
 {
-    while (cin >> n)
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+	while (cin >> n)
     {
-        l.resize(n);
-        rep(i, n) cin >> l[i];
-
-        sort(l.begin(), l.end());
-
-        
-    }
+        arr.clear();
+        arr.resize(n);
+		rep(i, n)
+        {
+			cin >> l;
+			arr[l - 1].push_back(0);
+		}
+		
+		ll ans = 0, d = 1;
+		rep(i, n)
+        {
+			forall(it, arr[n - i - 1]) if (!*it) ans += *it = d;
+			if (i < n - 1) rep(j, arr[n - i - 1].size() / 2)
+            {
+				arr[n - i - 2].push_back(arr[n - i - 1][2 * j] + arr[n - i - 1][2 * j + 1]);
+				d = max({d, arr[n - i - 1][2 * j], arr[n - i - 1][2 * j + 1]});
+			}
+		}
+		
+        cout << ans << '\n';
+	}
 }
