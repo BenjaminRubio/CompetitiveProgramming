6ce7a4b9d0161f2b6172b74b963cca9ef08c3c10
BenjaminRubio <berubio@uc.cl>
2020-04-02 17:50:43 -0300

    update 02/04/2020

--- Problems/Codeforces/TheMeetingPlaceCannotBeChanged.cpp
+++ Problems/Codeforces/TheMeetingPlaceCannotBeChanged.cpp
@@ -12,29 +12,24 @@ int main()
 
     cin >> n;
 
-    x.resize(n);
-    v.resize(n);
+    x.resize(n); v.resize(n);
     rep(i, n) cin >> x[i];
     rep(i, n) cin >> v[i];
 
-    double l = 0;
-    double r = 1e9 + 1;
+    double l = 0, r = 1e9 + 1;
     rep(i, 1000)
     {
         double m = (l + r) * 0.5;
 
-        double L = DBL_MIN;
-        double R = DBL_MAX;
+        double L = DBL_MIN, R = DBL_MAX;
         rep(i, n)
         {
             L = max(L, x[i] - v[i] * m);
             R = min(R, x[i] + v[i] * m);
         }
 
-        if (R >= L)
-            r = m;
-        else
-            l = m;
+        if (R >= L) r = m;
+        else l = m;
     }
 
     cout << (l + r) * 0.5 << '\n';
