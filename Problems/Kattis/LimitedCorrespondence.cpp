98ff38031df5558b0e2aeefc45123d4492626bb3
BenjaminRubio <berubio@uc.cl>
2020-04-18 01:42:12 -0400

    update 18/04/2020

--- Problems/Kattis/LimitedCorrespondence.cpp
+++ Problems/Kattis/LimitedCorrespondence.cpp
@@ -3,28 +3,71 @@ using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
 
-int N;
-string a, b;
+int N, m;
+string a, b, ans;
 vector<string> A, B;
 vector<bool> D;
 
-void solve(int i)
+bool check(int i)
 {
-    
+    auto na = a.size(), nb = b.size();
+    if (a.size() > b.size())
+    {
+        rep(j, min(na - nb, B[i].size()))
+            if (a[j + nb] != B[i][j]) return false;
+        if (na - nb > B[i].size()) return true;
+        rep(j, min(A[i].size(), B[i].size() - na + nb)) 
+            if (A[i][j] != B[i][j + na - nb]) return false;
+    }
+    else
+    {
+        rep(j, min(nb - na, A[i].size()))
+            if (b[j + na] != A[i][j]) return false;
+        if (nb - na > A[i].size()) return true;
+        rep(j, min(B[i].size(), A[i].size() - nb + na)) 
+            if (B[i][j] != A[i][j + nb - na]) return false;
+    }
+    return true;
+}
+
+void solve(int id)
+{
+    if (id and a.size() == b.size())
+    {
+        if (a.size() < m) { m = a.size(); ans = a; }
+        else if (a.size() == m and a < ans) ans = a;
+        return;
+    }
+    if (id == N) return;
+
+    rep(i, N) if (!D[i] and check(i))
+    {
+        D[i] = true;
+        a.append(A[i]); b.append(B[i]);
+        solve(id + 1);
+        a.erase(a.size() - A[i].size(), A[i].size());
+        b.erase(b.size() - B[i].size(), B[i].size());
+        D[i] = false;
+    }
 }
 
 int main()
 {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int T = 1;
     while (cin >> N)
     {
-        rep(i, N)
-        {
-            cin >> a >> b;
-            A.push_back(a); B.push_back(b);
-        }
+        cout << "Case " << T++ << ": ";
+        D.clear();
 
-        a.clear(); b.clear();
+        A.resize(N); B.resize(N);
+        rep(i, N) cin >> A[i] >> B[i];
+
+        a.clear(); b.clear(); m = 1e9;
+        D.assign(N, false);
         solve(0);
-        
+
+        if (m != 1e9) cout << ans << '\n';
+        else cout << "IMPOSSIBLE\n";
     }
 }
