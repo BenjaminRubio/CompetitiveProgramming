23e2a598741e20bfc3f7acf2d199e66b17cb8fa1
BenjaminRubio <berubio@uc.cl>
2020-05-13 02:24:12 -0400

    update 13/05/2020

--- Problems/Kattis/AbstractArt.cpp
+++ Problems/Kattis/AbstractArt.cpp
@@ -1,4 +1,4 @@
- #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
---
+++ Problems/SPOJ/ConvexPolygons.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define repx(i, a, b) for (int i = (int)a; i < (int)b; i++)
+
+typedef int T;
+struct P
+{
+    T x, y;
+    P() {}
+    P(T x, T y) : x(x), y(y) {}
+
+    P operator-(const P &p) const { return P(x - p.x, y - p.y); }
+    T operator^(const P &p) const { return x * p.y - y * p.x; }
+    bool operator<(const P &p) const { return y < p.y; }
+};
+istream &operator>>(istream &s, P &p) { return s >> p.x >> p.y; }
+
+int turn(P &a, P &b, P &c) { return (b - a) ^ (c - a); }
+
+const int MOD = 1234567;
+
+int C, N;
+vector<P> p;
+int DP[101][101][101];
+
+int dp(int i, int j, int k)
+{
+    if (DP[i][j][k] != -1) return DP[i][j][k];
+
+    int ans = 0;
+    repx(ii, i + 1, N) if (ii != j && ii != k)
+    {
+        if (turn(p[i], p[k], p[ii]) > 0 and turn(p[j], p[k], p[ii]) > 0)
+            ans = (ans + dp(i, k, ii) + 1) % MOD;
+    }
+    return DP[i][j][k] = ans;
+}
+
+int main()
+{
+    cin >> C;
+    while (C--)
+    {
+        memset(DP, -1, sizeof DP);
+
+        cin >> N;
+
+        p.resize(N);
+        rep(i, N) cin >> p[i];
+
+        sort(p.begin(), p.end());
+
+        int ans = 0;
+        rep(i, N) repx(j, i + 1, N) repx(k, j + 1, N)
+        {
+            if (turn(p[i], p[j], p[k]) > 0)
+                ans = (ans + dp(i, j, k) + 1) % MOD;
+            else ans = (ans + dp(i, k, j) + 1) % MOD;
+        }
+
+        cout << ans << '\n';
+    }
+}
