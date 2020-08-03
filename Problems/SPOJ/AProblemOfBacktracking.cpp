76f5c36113217f883734413262c4e8ef9b850279
BenjaminRubio <berubio@uc.cl>
2020-08-03 14:43:37 -0400

    update 03/08/2020

---
+++ Problems/SPOJ/AProblemOfBacktracking.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)n; i++)
+
+ll T, K, A[10], N[10];
+bool D[10];
+
+bool backtrack(ll i, ll s)
+{
+    if (s > K) return false;
+    if (i == 10 && s <= K) return true;
+
+    rep(d, 10) if (!D[d])
+    {
+        D[d] = 1, A[i] = d;
+        if (backtrack(i + 1, s + N[i] * d)) return true;
+        D[d] = 0;
+    }
+    return false;
+}
+
+int main()
+{
+    cin >> T;
+    while (T--)
+    {
+        rep(i, 10) cin >> N[i];
+        cin >> K;
+
+        memset(D, 0, sizeof D);
+
+        if (backtrack(0, 0))
+        {
+            rep(i, 10) cout << A[i] << ' ';
+            cout << '\n';
+        }
+        else cout << -1 << '\n';
+    }
+}
---
+++ Problems/SPOJ/LandAcquisition.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+#define repx(i, a, b) for (int i = (int)a; i < (int)b; i++)
+
+int main()
+{
+
+}
