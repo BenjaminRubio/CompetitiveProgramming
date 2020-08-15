be93d096b6fefd08b142e59097baf6cc3334e314
BenjaminRubio <berubio@uc.cl>
2020-08-14 22:44:02 -0400

    update 14/08/2020

--- Problems/AtCoder/Roadwork.cpp
+++ Problems/AtCoder/Roadwork.cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<int> vi;
-
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
-typedef set<pair<int, int>>::iterator iter;
 
 int N, Q, S, T, X, D;
-set<pair<int, pair<int, int>>> events;
-multiset<int> active;
-vi ans;
+set<pair<int, pair<int, int>>> events; // Ordena los eventos del sweep line
+multiset<int> active; // Guarda alturas ordenadas de segmentos activos
+vector<int> ans;
 
 int main()
 {
@@ -24,15 +21,10 @@ int main()
         events.insert({T - X, {0, X}});
         events.insert({S - X, {1, X}});
 
-        if (S - X < 0 && T - X >= 0)
-            active.insert(X);
+        if (S - X < 0 && T - X >= 0) active.insert(X);
     }
 
-    rep(i, Q)
-    {
-        cin >> D;
-        events.insert({D, {2, i}});
-    }
+    rep(i, Q) { cin >> D; events.insert({D, {2, i}}); }
 
     ans.assign(Q, -1);
 
@@ -41,13 +33,9 @@ int main()
         T = e.second.first;
         X = e.second.second;
 
-        if (T == 0)
-            active.erase(X);
-        if (T == 1)
-            active.insert(X);
-        if (T == 2)
-            if (!active.empty())
-                ans[X] = *active.begin();
+        if (T == 0) active.erase(X);
+        if (T == 1) active.insert(X);
+        if (T == 2 && !active.empty()) ans[X] = *active.begin();
     }
 
     rep(i, Q) cout << ans[i] << '\n';
