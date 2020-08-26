971c3e3f793e8e7d28a24f6dace55032cd26fd6c
BenjaminRubio <berubio@uc.cl>
2020-08-26 13:51:30 -0400

    update 26/08/2020

--- Problems/Codeforces/FourSegments.cpp
+++ Problems/Codeforces/FourSegments.cpp
@@ -12,44 +12,30 @@ int main()
     set<pair<pair<int, int>, pair<int, int>>> s;
 
     cin >> x >> y >> x_ >> y_;
-
-    if (x == x_ && y == y_) { cout << "NO\n"; return 0; }
     if (x == x_) V++;
     if (y == y_) H++;
 
     p.emplace(x, y); p.emplace(x_, y_);
     s.emplace(mp(x, y), mp(x_, y_));
     s.emplace(mp(x_, y_), mp(x, y));
-
-    if (s.size() < 2) { cout << "NO\n"; return 0; }
 
     cin >> x >> y >> x_ >> y_;
-    
-    if (x == x_ && y == y_) { cout << "NO\n"; return 0; }
     if (x == x_) V++;
     if (y == y_) H++;
 
     p.emplace(x, y); p.emplace(x_, y_);
     s.emplace(mp(x, y), mp(x_, y_));
     s.emplace(mp(x_, y_), mp(x, y));
-
-    if (s.size() < 4) { cout << "NO\n"; return 0; }
 
     cin >> x >> y >> x_ >> y_;
-    
-    if (x == x_ && y == y_) { cout << "NO\n"; return 0; }
     if (x == x_) V++;
     if (y == y_) H++;
 
     p.emplace(x, y); p.emplace(x_, y_);
     s.emplace(mp(x, y), mp(x_, y_));
     s.emplace(mp(x_, y_), mp(x, y));
-
-    if (s.size() < 6) { cout << "NO\n"; return 0; }
 
     cin >> x >> y >> x_ >> y_;
-    
-    if (x == x_ && y == y_) { cout << "NO\n"; return 0; }
     if (x == x_) V++;
     if (y == y_) H++;
 
@@ -57,9 +43,6 @@ int main()
     s.emplace(mp(x, y), mp(x_, y_));
     s.emplace(mp(x_, y_), mp(x, y));
 
-    if (s.size() < 8) { cout << "NO\n"; return 0; }
-
-    if (p.size() != 4 || H != 2 || V != 2) { cout << "NO\n"; return 0; }
-
-    cout << "YES\n";
+    if (s.size() != 8 || p.size() != 4 || H != 2 || V != 2) cout << "NO\n";
+    else cout << "YES\n";
 }
