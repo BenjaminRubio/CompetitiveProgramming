f2dfbda08ac9413b45f3155a7d818237ff792c2e
BenjaminRubio <berubio@uc.cl>
2020-09-17 21:45:46 -0300

    update 17/09/2020

--- Problems/Kattis/WipeYourWhiteboards.cpp
+++ Problems/Kattis/WipeYourWhiteboards.cpp
@@ -2,87 +2,45 @@
 using namespace std;
 
 typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> par;
-typedef vector<int> vi;
-typedef vector<float> vfloat;
-typedef vector<par> vp;
-typedef vector<vi> graph;
-typedef vector<vp> wgraph;
-
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
-#define repx(i, a, b) for (int i = a; i < (int)b; i++)
-#define invrep(i, a, b) for (int i = a; i > (int)b; i--)
 
-#define pb push_back
-#define pf push_front
-#define eb emplace_back
-#define ppb pop_back
-
-#define umap unordered_map
-
-#define lg(x) (31 - __buitlin_clz(x))
-#define lgg(x) (63 - __buitlin_clzll(x))
-#define gcd __gcd
-
-//ios::sync_with_stdio(0); cin.tie(0);
-//cout.setf(ios::fixed); cout.precision(4);
-
-#define debugx(x) // cerr << #x << ": " << x << endl
-#define debugv(v)         \
-    cerr << #v << ":";    \
-    for (auto e : v)      \
-        cerr << " " << e; \
-    cerr << endl
-#define debugm(m)                                        \
-    cerr << #m << endl;                                  \
-    rep(i, (int)m.size())                                \
-    {                                                    \
-        cerr << i << ":";                                \
-        rep(j, (int)m[i].size()) cerr << " " << m[i][j]; \
-        cerr << endl;                                    \
-    }
-#define debugmp(m) //                                                                       \
-    cerr << #m << endl;                                                                         \
-    rep(i, (int)m.size())                                                                       \
-    {                                                                                           \
-        cerr << i << ":";                                                                       \
-        rep(j, (int)m[i].size()) cerr << " {" << m[i][j].first << "," << m[i][j].second << "}"; \
-        cerr << endl;                                                                           \
-    }
-#define print(x) copy(x.begin(), x.end(), ostream_iterator<int>(cout, “”)), cout << endl
-
-ll gcdext(ll a, ll b, ll &x, ll &y)
-{
+ll gcdext(ll a, ll b, ll& x, ll& y) {
     ll r2, x2, y2, r1, x1, y1, r0, x0, y0, q;
-    r2 = a, x2 = 1, y2 = 0;
-    r1 = b, x1 = 0, y1 = 1;
-    while (r1)
-    {
-        q = r2 / r1;
-        r0 = r2 % r1;
-        x0 = x2 - q * x1;
-        y0 = y2 - q * y1;
-        r2 = r1, x2 = x1, y2 = y1;
-        r1 = r0, x1 = x0, y1 = y0;
+    r2 = a, x2 = 1, y2 = 0, r1 = b, x1 = 0, y1 = 1;
+    while (r1) {
+        q = r2 / r1, r0 = r2 % r1;
+        x0 = x2 - q * x1, y0 = y2 - q * y1;
+        r2 = r1, x2 = x1, y2 = y1, r1 = r0, x1 = x0, y1 = y0;        
     }
-    ll g = r2;
-    x = x2, y = y2;
-    if (g < 0)
-        g = -g, x = -x, y = -y;
+    ll g = r2; x = x2, y = y2;
+    if (g < 0) g = -g, x = -x, y = -y;
     return g;
 }
 
-int T, r, s, q;
+ll T, R, S, Q, X, Y, G, s1, s2, t;
 
 int main()
 {
     cin >> T;
-    rep(t, T)
+    while (T--)
     {
-        cin >> r >> s >> q;
-        ll x, y;
-        ll g = gcdext(r, s, x, y);
-        cerr << x * (q / g) << ' ' << y * (q / g) << '\n';
+        cin >> R >> S >> Q;
+
+        G = gcdext(R, S, X, Y);
+
+        X *= Q / G, Y *= Q / G;
+
+        ll M = R * -S / __gcd(R, -S);
+
+        if (X <= 0 || Y <= 0)
+        {
+            t = max(-X / (M / R), -Y / (M / -S)) + 1ll;
+            X += t * (M / R), Y += t * (M / -S);
+        }
+
+        t = min((X - 1ll) / (M / R), (Y - 1ll) / (M / -S));
+        X -= t * (M / R), Y -= t * (M / -S);
+        
+        cout << X << ' ' << Y << '\n';
     }
 }
