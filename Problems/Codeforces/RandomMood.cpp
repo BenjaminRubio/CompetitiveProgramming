fcfc6c76d6300590b20635e43b9bccdd43d3459d
BenjaminRubio <berubio@uc.cl>
2020-09-18 19:26:20 -0300

    update 18/09/2020

--- Problems/Codeforces/RandomMood.cpp
+++ Problems/Codeforces/RandomMood.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef vector<vector<double>> matrix;
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
 
 int N; double P;
+
+matrix mul(matrix &A, matrix &B)
+{
+    matrix ans = {{0, 0}, {0, 0}};
+    rep(i, 2) rep(j, 2) rep(k, 2) ans[i][k] += A[i][j] * B[j][k];
+    return ans;
+}
 
 int main()
 {
@@ -12,13 +20,13 @@ int main()
 
     cin >> N >> P;
 
-    double ans = 1;
+    matrix M = {{1 - P, P}, {P, 1 - P}}, A = {{1, 0}, {0, 1}};
     while (N)
     {
-        if (N & 1) ans = ans * (1 - P) + (1 - ans) * P;
-        P = P * (1 - P) + (1 - P) * P;
+        if (N & 1) A = mul(A, M);
+        M = mul(M, M);
         N >>= 1;
     }
 
-    cout << ans << '\n';
+    cout << A[0][0] << '\n';
 }
