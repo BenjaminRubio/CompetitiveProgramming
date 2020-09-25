e84adfab6c0c2411b4fd53e6f6851d3896e470e8
BenjaminRubio <berubio@uc.cl>
2020-09-25 20:30:34 -0300

    update 25/09/2020

--- Material/Structures/UnionFind.cpp
+++ Material/Structures/UnionFind.cpp
@@ -3,19 +3,18 @@ using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
 
-
 struct DSU
 {
-	vector<int> p;
+    vector<int> p;
     DSU(int N) : p(N, -1) {}
-	int get(int x) { return p[x] < 0 ? x : p[x] = get(p[x]); } 
-	bool sameSet(int a, int b) { return get(a) == get(b); }
-	int size(int x) { return -p[get(x)]; }
-	void unite(int x, int y)
+    int get(int x) { return p[x] < 0 ? x : p[x] = get(p[x]); } 
+    bool sameSet(int a, int b) { return get(a) == get(b); }
+    int size(int x) { return -p[get(x)]; }
+    void unite(int x, int y)
     {
-		x = get(x), y = get(y);
+        x = get(x), y = get(y);
         if (x == y) return;
-		if (p[x] > p[y]) swap(x,y);
-		p[x] += p[y], p[y] = x;
-	}
+        if (p[x] > p[y]) swap(x,y);
+        p[x] += p[y], p[y] = x;
+    }
 };
---
+++ Problems/Codeforces/EdgesInMST.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
+
+int N, M, u, v, w;
+vector<vector<int>> G, G_;
+
+int main()
+{
+
+}
