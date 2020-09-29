d4d97a0b7dc86b4cd8c60c9b6553fa6598b16610
BenjaminRubio <berubio@uc.cl>
2020-09-29 16:32:57 -0300

    update 29/09/2020

--- Problems/UVA/TransportationSystem.cpp
+++ Problems/UVA/TransportationSystem.cpp
@@ -32,40 +32,6 @@ void dfs(int i)
     rep(j, n) if (!visited[j] and P[i].dist(P[j]) <= r) dfs(j);
 }
 
-pair<double, double> find_mst() // Prim
-{
-    visited.assign(n, false);
-
-    double cost1 = 0, cost2 = 0;
-    int count = 1;
-    visited[0] = true;
-
-    priority_queue<Edge, vector<Edge>, greater<Edge>> q;
-    rep(i, n - 1) q.emplace(0, i + 1, P[0].dist(P[i + 1]),
-                            (P[0].dist(P[i + 1]) <= r) ? 0 : 1);
-
-    while (!q.empty())
-    {
-        Edge edge = q.top(); q.pop();
-
-        if (visited[edge.v]) continue;
-
-        int u = edge.u, v = edge.v, t = edge.t;
-        double cost = edge.w;
-        // cerr << u << ' ' << v << ' ' << cost << ' ' << t << '\n';
-        visited[v] = true;
-        if (t) cost2 += cost;
-        else cost1 += cost;
-
-        if (++count == n) break;
-
-        rep(i, n) if (!visited[i])
-            q.emplace(v, i, P[i].dist(P[v]), (P[i].dist(P[v]) <= r) ? 0 : 1);
-    }
-    
-    return {cost1, cost2};
-}
-
 int main()
 {
     ios::sync_with_stdio(0);
@@ -83,9 +49,35 @@ int main()
         visited.assign(n, false);
         rep(i, n) if (!visited[i]) { dfs(i); c++; }
 
-        pair<double, double> ans = find_mst();
+        visited.assign(n, false);
+
+        double cost1 = 0, cost2 = 0;
+        int count = 1;
+        visited[0] = true;
+
+        priority_queue<Edge, vector<Edge>, greater<Edge>> q;
+        rep(i, n - 1) q.emplace(0, i + 1, P[0].dist(P[i + 1]),
+                                (P[0].dist(P[i + 1]) <= r) ? 0 : 1);
+
+        while (!q.empty())
+        {
+            Edge edge = q.top(); q.pop();
+
+            if (visited[edge.v]) continue;
+
+            int u = edge.u, v = edge.v, t = edge.t;
+            double cost = edge.w;
+            visited[v] = true;
+            if (t) cost2 += cost;
+            else cost1 += cost;
+
+            if (++count == n) break;
+
+            rep(i, n) if (!visited[i])
+                q.emplace(v, i, P[i].dist(P[v]), (P[i].dist(P[v]) <= r) ? 0 : 1);
+        }
 
         cout << "Case #" << _ + 1 << ": ";
-        cout << c << ' ' << round(ans.first) << ' ' << round(ans.second) << '\n';
+        cout << c << ' ' << round(cost1) << ' ' << round(cost2) << '\n';
     }
 }
