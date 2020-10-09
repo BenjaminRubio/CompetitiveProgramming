4ac9dd6ed66821bbe10da03605dcceead12a78e8
BenjaminRubio <berubio@uc.cl>
2020-10-09 18:30:37 -0300

    update 09/10/2020

--- Material/Strings/KMP.cpp
+++ Material/Strings/KMP.cpp
@@ -8,12 +8,10 @@ int match(string &p, string &t)
     int n = p.size(), m = t.size(), L[n]; L[0] = 0;
     rep(j, n - 1)
     {
-        int i = L[j];
-        while (p[i] != p[j + 1] && i) i = L[i - 1];
+        int i = L[j]; while (p[i] != p[j + 1] && i) i = L[i - 1];
         L[j + 1] = p[i] == p[j + 1] ? i + 1 : 0;
     }
-    int ans = 0, i = 0;
-    rep(j, m)
+    int ans = 0, i = 0; rep(j, m)
     {
         while (p[i] != t[j] && i) i = L[i - 1];
         if (p[i] == t[j] && ++i == n) i = L[n - 1], ans++;
