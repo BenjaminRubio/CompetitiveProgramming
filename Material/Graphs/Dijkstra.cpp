2a567810610aa867b5726e93641a1bcb87d05beb
BenjaminRubio <berubio@uc.cl>
2020-10-13 01:02:01 -0300

    update 13/10/2020

--- Material/Graphs/Dijkstra.cpp
+++ Material/Graphs/Dijkstra.cpp
@@ -24,10 +24,7 @@ int dijkstra(int s, int t)
         int u = q.top().v, w = q.top().w; q.pop();  // CHECK FOR OVERFLOW
         if (C[u] < w) continue;
         for (auto e : G[u]) if (C[e.v] > e.w + w)
-        {
-            C[e.v] = e.w + w;
-            q.emplace(e.v, C[e.v]);
-        }
+            C[e.v] = e.w + w, q.emplace(e.v, C[e.v]);
     }
     return C[t];
 }
