099517fb460214af97a604854baaa665a0ba42e6
BenjaminRubio <berubio@uc.cl>
2020-11-11 21:36:00 -0300

    update 11/11/2020

--- Problems/SPOJ/LeftRightOrCenter.cpp
+++ Problems/SPOJ/LeftRightOrCenter.cpp
@@ -2,86 +2,27 @@
 using namespace std;
 
 typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> par;
-typedef vector<int> vi;
-typedef vector<float> vfloat;
-typedef vector<par> vp;
-typedef vector<vi> graph;
-typedef vector<vp> wgraph;
 
-#define rep(i, n) for (int i = 0; i < (int)n; i++)
-#define repx(i, a, b) for (int i = a; i < (int)b; i++)
-#define invrep(i, b, a) for (int i = b; i >= a; --i)
-
-#define pb push_back
-#define pf push_front
-#define eb emplace_back
-#define ppb pop_back
-
-#define umap unordered_map
-
-#define lg(x) (31 - __builtin_clz(x))
-#define lgg(x) (63 - __builtin_clzll(x))
-#define gcd __gcd
-
-//ios::sync_with_stdio(0); cin.tie(0);
-//cout.setf(ios::fixed); cout.precision(4);
-
-#define debugx(x) cerr << #x << ": " << x << endl
-#define debugv(v)         \
-    cerr << #v << ":";    \
-    for (auto e : v)      \
-        cerr << " " << e; \
-    cerr << endl
-#define debugm(m)                                        \
-    cerr << #m << endl;                                  \
-    rep(i, (int)m.size())                                \
-    {                                                    \
-        cerr << i << ":";                                \
-        rep(j, (int)m[i].size()) cerr << " " << m[i][j]; \
-        cerr << endl;                                    \
-    }
-#define debugmp(m) //\
-    cerr << #m << endl;                                                                         \
-    rep(i, (int)m.size())                                                                       \
-    {                                                                                           \
-        cerr << i << ":";                                                                       \
-        rep(j, (int)m[i].size()) cerr << " {" << m[i][j].first << "," << m[i][j].second << "}"; \
-        cerr << endl;                                                                           \
-    }
-#define print(x) copy(x.begin(), x.end(), ostream_iterator<int>(cout, “”)), cout << endl
-
-template <typename T>
-struct Point
+typedef ll T; struct P
 {
     T x, y;
+    P() {} P(T x, T y) : x(x), y(y) {}
 
-    Point<T> operator-(const Point<T>& p) const { return {x-p.x, y-p.y}; }
-
-    T cross(const Point<T> &p) const { return x * p.y - y * p.x; }
+    P operator-(const P &p) const { return P(x - p.x, y - p.y); }
+    T operator^(const P &p) const { return x * p.y - y * p.x; }
 };
+istream &operator>>(istream &s, P &p) { return s >> p.x >> p.y; }
 
-int x, y, n;
+int N; P a, b, p;
 
 int main()
 {
-    cin >> x >> y;
-    Point<ll> orig = {x, y};
-    
-    cin >> x >> y;
-    Point<ll> guide = {x, y};
-
-    cin >> n;
-    rep(i, n)
+    cin >> a >> b >> N;
+    while (N-- && cin >> p)
     {
-        cin >> x >> y;
-        ll dir = (guide - orig).cross(Point<ll>({x, y}) - orig);
-        if (dir > 0)
-            cout << "I\n";
-        else if (dir < 0)
-            cout << "D\n";
-        else
-            cout << "C\n";
+        ll dir = (b - a) ^ (p - a);
+        if (dir > 0) cout << "I\n";
+        else if (dir < 0) cout << "D\n";
+        else cout << "C\n";
     }
 }
