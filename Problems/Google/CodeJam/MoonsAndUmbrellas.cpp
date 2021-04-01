49da6e5020af105c4941f04f370cebce6e7c6431
BenjaminRubio <berubio@uc.cl>
2021-04-01 15:44:42 -0300

    update 01/04/21

--- Problems/Google/CodeJam/MoonsAndUmbrellas.cpp
+++ Problems/Google/CodeJam/MoonsAndUmbrellas.cpp
@@ -3,8 +3,26 @@ using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
 
-int T, X, Y;
+int T, N, X, Y;
 string S;
+
+int DP[1010][20];
+bool D[1010][20];
+
+int dp(int i, int p)
+{
+    if (i == N) return 0;
+    if (D[i][p]) return DP[i][p];
+
+    int ans;
+    if (S[i] != '?') ans = dp(i + 1, S[i]);
+    if (S[i] == 'J' && p == 'C') ans += X;
+    if (S[i] == 'C' && p == 'J') ans += Y;
+    if (S[i] == '?') ans = min(dp(i + 1, 'J') + (p == 'C' ? X : 0), dp(i + 1, 'C') + (p == 'J' ? Y : 0));
+
+    D[i][p] = 1;
+    return DP[i][p] = ans;
+}
 
 int main()
 {
@@ -13,18 +31,9 @@ int main()
     {
         cout << "Case #" << t + 1 << ": ";
 
-        cin >> X >> Y >> S;
+        cin >> X >> Y >> S; N = S.size();
 
-        int ans = 0;
-
-        char l = -1;
-        for (char c : S)
-        {
-            if (c == 'J' && l == 'C') ans += X;
-            if (c == 'C' && l == 'J') ans += Y;
-            if (c != '?') l = c;
-        }
-
-        cout << ans << '\n';
+        memset(D, 0, sizeof D);
+        cout << dp(0, 0) << '\n';
     }
 }
