e5b9b86f8ba9ea3ac5244baa7edb0c2a0158e341
BenjaminRubio <berubio@uc.cl>
2021-04-20 19:25:34 -0400

    update 20/04/21

--- Problems/Codeforces/PolicePatrol.cpp
+++ Problems/Codeforces/PolicePatrol.cpp
@@ -1,36 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, n) for (int i = 0; i < (int)n; i++)
 typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (int)n; i++)
 
-int n, m;
-vector<int> p;
+int N, M;
+vector<int> P;
 
 ll calc(int x)
 {
     ll ans = 0;
-    for (int i = 0; i < x; i += m) ans += 2ll * (p[x] - p[i]);
-    for (int i = n - 1; i >= x; i -= m) ans += 2ll * (p[i] - p[x]);
+    for (int i = 0; i < x; i += M) ans += 2ll * (P[x] - P[i]);
+    for (int i = N - 1; i >= x; i -= M) ans += 2ll * (P[i] - P[x]);
     return ans;
 }
 
 int main()
 {
-    cin >> n >> m;
+    ios::sync_with_stdio(0); cin.tie(0);
 
-    p.resize(n);
-    rep(i, n) cin >> p[i];
+    cin >> N >> M;
 
-    int l = 0, r = n - 1;
-    while (r - l > 2)
+    P.resize(N);
+    rep(i, N) cin >> P[i];
+
+    int l = 0, r = N - 1;
+    while (r - l > 10)
     {
         int m1 = (2 * l + r) / 3;
         int m2 = (l + 2 * r) / 3;
 
-        if (calc(m1) > calc(m2)) l = m1;
-        else if (calc(m2) > calc(m1)) r = m2;
-        else l = m1, r = m2;
+        ll f1 = calc(m1), f2 = calc(m2);
+
+        if (f1 >= f2) l = m1;
+        if (f2 >= f1) r = m2;
     }
-    cout << min(calc(l), calc(r)) << '\n';
+    ll ans = calc(l);
+    for (int k = l; k <= r; k++) ans = min(ans, calc(k));
+    cout << ans << '\n';
 }
