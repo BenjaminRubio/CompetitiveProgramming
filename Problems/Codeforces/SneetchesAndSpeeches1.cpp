38d22b3b24bf8793cac541d619ea80b7b161045f
BenjaminRubio <berubio@uc.cl>
2021-04-21 20:43:27 -0400

    update 21/04/21

--- Problems/Codeforces/SneetchesAndSpeeches1.cpp
+++ Problems/Codeforces/SneetchesAndSpeeches1.cpp
@@ -1,111 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-mt19937 gen(chrono::high_resolution_clock::now().time_since_epoch().count());
-
 #define rep(i, n) for (int i = 0; i < (int)n; i++)
-typedef pair<int, int> ii;
-#define ff first
-#define ss second
 
 struct Node
 {
-    int p, sz = 0, v, m0l = 0, m0r = 0 , m1l = 0, m1r = 0, m = 0, l = -1, r = -1;
-    bool lz = false, f = false;
+    int sz = 0, m0l = 0, m0r = 0 , m1l = 0, m1r = 0, m = 0;
+    bool lz = false;
     Node() {}
-    Node(int x): p(gen()), sz(1), v(x), m(1)
+    Node(int x) : sz(1), m(1)
     {
         m0l = m0r = (x == 0);
         m1l = m1r = (x == 1);
     }
-    void recalc(const Node &a, const Node &b)
+    Node(const Node &a, const Node &b)
     {
-        sz = a.sz + b.sz + 1; m = max(a.m, b.m);
+        sz = a.sz + b.sz;
+        m = max({a.m, b.m, a.m0r + b.m0l, a.m1r + b.m1l});
         m0l = a.m0l, m1l = a.m1l, m0r = b.m0r, m1r = b.m1r;
-        if (v == 0) m = max(m, a.m0r + 1 + b.m0l);
-        if (v == 1) m = max(m, a.m1r + 1 + b.m1l);
-        if (a.m0r == a.sz && v == 0) m0l = a.sz + 1 + b.m0l;
-        if (a.m1r == a.sz && v == 1) m1l = a.sz + 1 + b.m1l;
-        if (b.m0l == b.sz && v == 0) m0r = b.sz + 1 + a.m0r;
-        if (b.m1l == b.sz && v == 1) m1r = b.sz + 1 + a.m1r;
+        if (a.m0l == a.sz) m0l = a.sz + b.m0l;
+        if (a.m1l == a.sz) m1l = a.sz + b.m1l;
+        if (b.m0r == b.sz) m0r = b.sz + a.m0r;
+        if (b.m1r == b.sz) m1r = b.sz + a.m1r;
     }
-    void upd_lazy() { lz ^= 1; }
-    void lazy() { v ^= 1, swap(m0l, m1l), swap(m0r, m1r), lz = 0; }
+    Node(const Node &b)
+    {
+        sz = b.sz; m = b.m;
+        m0l = b.m1l, m1l = b.m0l;
+        m0r = b.m1r, m1r = b.m0r;
+    }
 };
 
+// 0 - indexed / inclusive - exclusive
 template <class node>
-struct Treap
+struct STL
 {
-    vector<node> t; int n, r = -1;
+    vector<node> st; int n;
 
-    node get(int u) { return u != -1 ? t[u] : node(); }
-    void recalc(int u)
+    void build(int u, int i, int j, vector<node> &arr)
     {
-        int l = t[u].l, r = t[u].r;
-        push(l); push(r);
-        t[u].recalc(get(l), get(r));
+        if (i == j) { st[u] = arr[i]; return; }
+        int m = (i + j) / 2, l = u * 2 + 1, r = u * 2 + 2;
+        build(l, i, m, arr), build(r, m + 1, j, arr);
+        st[u] = node(st[l], st[r]);
     }
-    void push(int u)
+    void push(int u, int i, int j)
     {
-        if (u == -1 || !t[u].lz) return;
-        int l = t[u].l, r = t[u].r;
-        if (l != -1) t[l].upd_lazy();
-        if (r != -1) t[r].upd_lazy();
-        t[u].lazy();
+        st[u] = node(st[u]);
+        if (i == j) return;
+        st[u * 2 + 1].lz ^= 1;
+        st[u * 2 + 2].lz ^= 1;
     }
-    int merge(int l, int r)
+    node query(int a, int b, int u, int i, int j)
     {
-        if (min(l, r) == -1) return l != -1 ? l : r;
-        push(l); push(r);
-        int ans = (t[l].p < t[r].p) ? l : r;
-        if (ans == l) t[l].r = merge(t[l].r, r), recalc(l);
-        if (ans == r) t[r].l = merge(l, t[r].l), recalc(r);
-        return ans;
+        if (j < a || b < i) return node();
+        int m = (i + j) / 2, l = u * 2 + 1, r = u * 2 + 2;
+        if (st[u].lz) push(u, i, j);
+        if (a <= i && j <= b) return st[u];
+        return node(query(a, b, l, i, m), query(a, b, r, m + 1, j));
     }
-    ii split(int u, int id)
+    void update(int a, int b, int u, int i, int j)
     {
-        if (u == -1) return {-1, -1};
-        push(u);
-        if (get(t[u].l).sz >= id)
-        {
-            ii ans = split(t[u].l, id);
-            t[u].l = ans.ss; recalc(u);
-            return {ans.ff, u};
-        }
-        id -= get(t[u].l).sz + 1;
-        ii ans = split(t[u].r, id);
-        t[u].r = ans.ff; recalc(u);
-        return {u, ans.ss};
-    }
-    int update(int u, int l, int r)
-    {
-        if (l > r) return u;
-        ii a = split(u, l), b = split(a.ss, r - l + 1);
-        t[b.ff].upd_lazy();
-        return merge(a.ff, merge(b.ff, b.ss));
+        if (st[u].lz) push(u, i, j);
+        if (j < a || b < i) return;
+        int m = (i + j) / 2, l = u * 2 + 1, r = u * 2 + 2;
+        if (a <= i && j <= b) { push(u, i, j); return; }
+        update(a, b, l, i, m); update(a, b, r, m + 1, j);
+        st[u] = node(st[l], st[r]);
     }
 
-    Treap(vector<int> &v) : n(v.size())
-    { rep(i, n) t.emplace_back(v[i]), r = merge(r, i); }
+    STL(vector<node> &v, int N) : n(N), st(N * 4 + 5) { build(0, 0, n - 1, v); }
+    node query(int a, int b) { return query(a, b, 0, 0, n - 1); }
+    void update(int a, int b) { update(a, b, 0, 0, n - 1); }
 };
 
 int N, Q, t, l, r; char c;
-vector<int> v;
+vector<Node> v;
 
 int main()
 {
     cin >> N >> Q;
 
     v.resize(N);
-    rep(i, N) { cin >> c; v[i] = c - '0'; }
+    rep(i, N) { cin >> c; v[i] = Node(c - '0'); }
 
-    Treap<Node> treap(v);
-    int root = treap.r;
+    STL<Node> stl(v, N);
 
     while (Q--)
     {
         cin >> t >> l >> r; l--, r--;
-        root = treap.update(root, l, r);
-        cout << treap.t[root].m << '\n';
+        stl.update(l, r);
+        cout << stl.query(0, N - 1).m << '\n';
     }
 }
