5a99198fa5373a67b1218949e3c44d3474c78b23
BenjaminRubio <berubio@uc.cl>
2021-04-23 16:27:09 -0400

    update 23/04/21

--- Material/Math/Sieve.cpp
+++ Material/Math/Sieve.cpp
@@ -8,11 +8,10 @@ template<int SZ> struct Sieve
     bitset<SZ> I; vector<int> P;
     Sieve()
     {
-        I.set(); I[0] = I[1] = 0;
-        for (int i = 4; i < SZ; i += 2) I[i] = 0;
-        for (int i = 3; i * i < SZ; i += 2) if (I[i])
-            for (int j = i*i; j < SZ; j += i*2) I[j] = 0;
-        rep(i, SZ) if (I[i]) P.push_back(i);
+        I.set();
+        for (int i = 2; i * i < SZ; i++) if (I[i])
+            for (int j = i * i; j < SZ; j += i) I[j] = 0;
+        rep(i, SZ) if (i > 1 && I[i]) P.push_back(i);
     }
 };
 
