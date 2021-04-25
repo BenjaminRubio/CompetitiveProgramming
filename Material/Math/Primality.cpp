1058553140229a00b420aff64c260fdebedcd7b3
BenjaminRubio <berubio@uc.cl>
2021-04-24 22:20:30 -0400

    update 24/04/21

--- Material/Math/Primality.cpp
+++ Material/Math/Primality.cpp
@@ -2,14 +2,14 @@
 
 bool prime(ul n)
 {
-	if (n < 2 || n % 6 % 4 != 1) return n - 2 < 2;
-	ul A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    if (n < 2 || n % 6 % 4 != 1) return n - 2 < 2;
+    ul A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
     ul s = __builtin_ctzll(n - 1), d = n >> s;
-	for (int a : A)
+    for (int a : A)
     {
-		ul p = modPow(a, d, n), i = s;
-		while (p != 1 && p != n - 1 && a % n && i--) p = modMul(p, p, n);
-		if (p != n - 1 && i != s) return 0;
-	}
-	return 1;
+        ul p = modPow(a, d, n), i = s;
+        while (p != 1 && p != n - 1 && a % n && i--) p = modMul(p, p, n);
+        if (p != n - 1 && i != s) return 0;
+    }
+    return 1;
 }
