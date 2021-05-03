7b70296b71413457c28d9373cfddb2368df3b06f
BenjaminRubio <berubio@uc.cl>
2021-05-03 14:02:31 -0400

    update 03/05/21

--- Material/Structures/SparseTable.cpp
+++ Material/Structures/SparseTable.cpp
@@ -1,45 +1,36 @@
 
-template <class t>
-class ST
-{
-    int n; vi memo, *arr;
-
-public:
-    ST(vi &_arr)
-    {
-        arr = &_arr; n = arr->size();
-        int maxlog = 31 - __builtin_clz(n);
-        memo.assign(n * (maxlog + 1), -1);
-    }
-    int dp(int i, int e)
-    {
-        int &ans = memo[e * n + i];
-        if (ans != -1) return ans;
-        if (e == 0) return ans = (*arr)[i];
-        return ans = t::merge(dp(i, e - 1), dp(i + (1 << (e - 1)), e - 1));
-    }
-
-    int query_O1(int l, int r)
-    {
-        int e = 31 - __builtin_clz(r - l + 1);
-        return t::merge(dp(l, e), dp(r - (1 << e) + 1, e));
-    }
-
-    int query_Ologn(int l, int r)
-    {
-        int ans = t::neutro;
-        int d = r - l + 1;
-        for (int e = 0; d; e++, d >>= 1) if (d & 1)
-        {
-            ans = t::merge(ans, dp(l, e));
-            l += 1 << e;
-        }
-        return ans;
-    }
-};
-
 struct OP
 {
     static const int neutro = 0;
     static int merge(int x, int y) { return x + y; }
+};
+
+template <class t>
+class ST
+{
+    int N; vi DP;
+
+public:
+    ST(vi &v)
+    {
+        N = v.size(); int LOG = 31 - __builtin_clz(n);
+        DP.assign(N * (LOG + 1), -1);
+        rep(i, N) DP[i] = v[i];
+        rep(k, LOG) rep(i, N)
+            DP[(k + 1) * N + i] = t::merge(DP[k * N + i], 
+                                           DP(k * N + i + (1 << k)));
+    }
+    int query_O1(int l, int r)
+    {
+        int k = 31 - __builtin_clz(r - l + 1);
+        return t::merge(DP[k * N + l], DP[k * N + r - (1 << k) + 1];
+    }
+    int query_Ologn(int l, int r)
+    {
+        int ans = t::neutro;
+        int d = r - l + 1;
+        for (int k = 0; d; k++, k >>= 1) if (k & 1)
+            ans = t::merge(ans, DP[k * N + l]), l += 1 << e;
+        return ans;
+    }
 };
