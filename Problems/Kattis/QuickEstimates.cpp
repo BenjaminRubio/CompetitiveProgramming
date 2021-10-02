3bf294b94393b611a1c813522428ba2d07a170d9
BenjaminRubio <benja.rubio.o@gmail.com>
2021-10-02 00:57:48 -0300

    update 01/10

---
+++ Problems/Kattis/QuickEstimates.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N; string S;
+
+int main()
+{
+    cin >> N;
+    while (N--)
+    {
+        cin >> S;
+        cout << S.size() << '\n';
+    }
+}
